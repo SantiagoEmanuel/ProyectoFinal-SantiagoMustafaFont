2b511e64e0a192b218f3bc65b5578dc3b7adb266
SantiagoEmanuel_ <santiagomustafa3@gmail.com>
2024-02-16 14:52:37 -0300

    Actualización de la funcionalidad del proyecto, agrego funciones (Login, Register, GenerarPedido, CalcularIVA, PedirCUIT, TipoFactura e IniciarEmulador), actualizo la función GenerarFactura, creo la clase User, genero "Base de datos" para los usuarios y los productos disponibles para la compra. Mejora de la documentación.

--- README.md
+++ README.md
@@ -1,10 +1,50 @@
-# Primera entrega de la actividad obligatoria número 1
+# Pre-entrega número dos - Emulador de Compra y Facturación de Agua y Hielo
 
-## Generador de Facturas A y B
+Este proyecto es un emulador de compra y facturación para una planta de agua y hielo llamada "Agua Santa Ángela". Permite a los usuarios registrados realizar pedidos de productos disponibles y generar facturas correspondientes.
 
-> - Creamos una validación de la factura que se desea generar
-> - Creamos una función para optimizar la funcionalidad del código.
-> - Creamos dos funciones similares, una para la Factura A y otra para la Factura B
-> Si bien ésta última función puede simplificarse a una sola función he decidido entregarla así
-> - Agregamos los elementos necesarios para añadirlos al DOM.
-> - Agregamos los elementos al DOM.
+## Funcionalidades Principales
+
+- **Registro de Usuarios**: Los usuarios pueden registrarse ingresando su nombre, correo electrónico y contraseña.
+- **Inicio de Sesión**: Los usuarios pueden iniciar sesión con su correo electrónico y contraseña.
+- **Generación de Pedidos**: Los usuarios pueden seleccionar productos disponibles y especificar la cantidad deseada.
+- **Generación de Facturas**: Se generan facturas para los pedidos realizados, aplicando el tipo de factura seleccionado por el usuario.
+
+## Funciones Principales
+
+### `register(username, mail, password, passwordConfirmation)`
+
+Registra un nuevo usuario en la base de datos.
+
+### `login(mail, password)`
+
+Inicia sesión del usuario con el correo electrónico y contraseña proporcionados.
+
+### `generarPedido(user, products)`
+
+Genera el pedido del cliente (producto y cantidad) seleccionando de una lista de productos disponibles.
+
+### `tipoFactura()`
+
+Obtiene el tipo de factura (A o B) del usuario.
+
+### `pedirCuit()`
+
+Pide ingresar el CUIT (Clave Única de Identificación Tributaria) al usuario como dato para la factura.
+
+### `calcularIVA(precio)`
+
+Genera el valor del producto con IVA para la factura A.
+
+### `generarFactura(producto, cantidad, user, products)`
+
+Genera la factura A o B y es impresa en consola.
+
+### `iniciarEmulador(products)`
+
+Inicia la emulación de compra y facturación de un usuario en una planta de agua y hielo.
+
+## Uso
+
+1. Clona el repositorio o descarga el código.
+2. Enlaza el código a un archivo **HTML5** para ejecutarlo.
+3. Sigue las instrucciones en pantalla para registrarte, iniciar sesión, realizar un pedido y generar una factura.
--- js/main.js
+++ js/main.js
@@ -1,99 +1,176 @@
 // Evitamos magic strings
-const vacio = '';
-const a = 'A'
-const b = 'B'
+const vacio = "";
+const a = "A";
+const b = "B";
 
-// Función para Generar facturas
-function generarFactura(tipoFactura) {
+// "Base de datos" de los productos disponibles [no maneja stock aún].
+const products = [
+    { product: "Bidon de agua", litros: 20, price: 600 },
+    { product: "Bidon de agua", litros: 10, price: 400 },
+    { product: "Bolsa de hielo", kilos: 12, price: 700 },
+    { product: "Bolsa de hielo", kilos: 3, price: 300 },
+];
 
-    // Pedimos los datos para realizar la Factura.
-    cuil = prompt('Ingrese su CUIL')
-    nombre = prompt('Ingrese su nombre')
+// "Base de datos" de los usuarios registrados.
+const users = [];
 
-    // Comprobamos que los datos no estén vacíos.
-    while (cuil == vacio || nombre == vacio) {
-        if (cuil == vacio) {
-            cuil = prompt('Ingrese su CUIL')
-        } else {
-            nombre = prompt('Ingrese su nombre')
-        }
+// Clase Usuarios
+class User {
+    /**
+     * Constructor de la clase User
+     * @param {String} username - Nombre del usuario
+     * @param {String} mail - Correo electrónico del usuario
+     * @param {String} password - Contraseña del usuario
+     */
+    constructor(username, mail, password) {
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+    }
+}
+
+/**
+ * Registra un nuevo usuario en la base de datos.
+ * @param {string} username - Nombre de usuario
+ * @param {string} mail - Correo electrónico del usuario
+ * @param {string} password - Contraseña del usuario
+ * @param {string} passwordConfirmation - Confirmación de la contraseña del usuario
+ * @returns {string} - Nombre de usuario registrado
+ */
+function register(username, mail, password, passwordConfirmation) {
+    // Comprobamos que las contraseñas coincidan.
+    if (password === passwordConfirmation) {
+        // Ingresamos al usuario a la base de datos.
+        users.push(new User(username, mail, password));
+    } else {
+        // Avisamos que las contraseñas no coinciden.
+        alert("Los las contraseñas no coinciden.");
+        return;
     }
 
-    // Pedimos el nombre del producto.
+    // Iniciamos sesión.
+    login(mail, password);
+
+    return username;
+}
+
+/**
+ * Inicia sesión del usuario con el correo electrónico y contraseña proporcionados.
+ * @param {string} mail - Correo electrónico del usuario
+ * @param {string} password - Contraseña del usuario
+ */
+function login(mail, password) {
+    // Corroboramos que los datos ingresados coincidan con algún usuario en la base de datos.
+    if (users.some((e) => e.mail == mail && e.password == password)) {
+        alert(`Bienvenido!`);
+    } else {
+        alert("Lo datos ingresados están mal o no coinciden con ningún usuario.");
+    }
+}
+
+/**
+ * Genera el pedido del cliente (producto y cantidad).
+ * @param {string} user - Nombre del usuario
+ * @param {Array} products - Lista de productos disponibles
+ * @returns {Array} - Índice del producto seleccionado y cantidad
+ */
+function generarPedido(user, products) {
     do {
-        producto = prompt('Ingrese el producto')
-    } while (producto == vacio);
+        pedido = parseInt(
+            prompt(
+                `Bienvenido, ${user} a Agua Santa Ángela \n\nSelecciona tu pedido \n1)Bidón de Agua x 20lts \n2)Bidón de Agua x 10lts \n3)Bolsa de hielo x 12kg \n4)Bolsa de hielo x 3kg`
+            )
+        );
+    } while (pedido <= 0 || pedido >= 5);
 
-    // Pedimos que se ingrese el valor y cantidad del producto a facturar.
-    // Además corroboramos los datos ingresados.
     do {
-        precio = parseFloat(prompt('Ingrese el precio del producto'))
-        cantidad = parseInt(prompt('Ingrese la cantidad de productos'))
-    } while (precio == isNaN || cantidad == isNaN);
+        cantidad = parseInt(
+            prompt(`Ingrese la cantidad de ${products[pedido - 1].product} necesitas`)
+        );
+    } while (cantidad <= 0);
 
-    if (tipoFactura == a) facturaA(tipoFactura, cuil, nombre, producto, precio, cantidad)
-    else facturaB(tipoFactura, cuil, nombre, producto, precio, cantidad)
+    return [pedido - 1, cantidad];
 }
 
-// Generamos factura A
-function facturaA(tipoFactura, cuil, nombre, producto, precio, cantidad) {
+/**
+ * Obtiene el tipo de factura (A o B) del usuario.
+ * @returns {string} - Tipo de factura seleccionado
+ */
+function tipoFactura() {
+    do {
+        x = prompt("Ingresa el tipo de factura que deseas hacer (A o B)");
+        x = x.toUpperCase();
+    } while (x != a && x != b);
 
-    // Generamos el precio del producto con iva
-    const precioConIva = (precio * .21) + precio;
-
-    // Creamos los elementos.
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
-    const h2 = document.createElement('h2')
-    const p = document.createElement('p')
-    const p2 = document.createElement('p')
-
-    // Completamos los datos de cada elemento.
-    h2.innerHTML = `Factura ${tipoFactura}`
-    p.innerHTML = `CUIL-CUIT: ${cuil} | NOMBRE: ${nombre}`
-    p2.innerHTML = `PRODUCTO: ${producto} VALOR: ${(precioConIva)}|| TOTAL:${precioConIva * cantidad}.`
-
-    // Agregamos a los padres los contenedores hijos.
-    div2.append(h2)
-    div2.append(p)
-    div.append(div2)
-    div.append(p2)
-
-    // Recuperamos un elemento de la página.
-    const factura = document.getElementById('factura')
-
-    // Agregamos el elemento hijo.
-    factura.append(div)
+    return x;
 }
 
-// Generamos Factura B
-function facturaB(tipoFactura, cuil, nombre, producto, precio, cantidad) {
-    // Creamos los elementos
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
-    const h2 = document.createElement('h2')
-    const p = document.createElement('p')
-    const p2 = document.createElement('p')
+/**
+ * Pide ingresar el CUIT al usuario como dato para la Factura.
+ * @returns {String} - Número CUIT del usuario
+ */
+function pedirCuit() {
+    do {
+        cuit = prompt("Ingresa tu CUIT");
+    } while (cuit == vacio);
 
-    // Agregamos contenido a los elementos.
-    h2.innerHTML = `Factura ${tipoFactura}`
-    p.innerHTML = `CUIL-CUIT: ${cuil} | NOMBRE: ${nombre}`
-    p2.innerHTML = `${producto} a ${precio} c/u || ${precio * cantidad}`
-
-    // Agregamos a los padres los contenedores hijos.
-    div2.append(h2)
-    div2.append(p)
-    div.append(div2)
-    div.append(p2)
-
-    // Recuperamos un elemento de la página.
-    const factura = document.getElementById('factura')
-
-    // Agregamos el elemento hijo.
-    factura.append(div)
+    return cuit;
 }
-do {
-    tipoFactura = prompt("Ingrese el tipo de factura que desea realizar (Utilizar mayúsculas)")
-} while (tipoFactura == vacio || (tipoFactura != a && tipoFactura != b));
 
-generarFactura(tipoFactura)
+/**
+ * Genera el valor del producto con IVA para la factura A.
+ * @param {Number} precio - valor del producto sin IVA
+ * @returns {Number} - Valor del producto más IVA
+ */
+function calcularIVA(precio) {
+    return precio * 0.21 + precio;
+}
+
+/**
+ * Genera la factura A o B y es impresa en consola.
+ * @param {Number} producto - Índice del producto seleccionado
+ * @param {Number} cantidad - Cantidad de productos requeridos por el usuario
+ * @param {String} user - Nombre del usuario
+ * @param {Array} products - Lista de productos disponibles
+ */
+function generarFactura(producto, cantidad, user, products) {
+    const factura = tipoFactura();
+
+    // Pedimos los datos para realizar la Factura.
+    const cuit = pedirCuit();
+
+    if (factura == a) {
+        const precioConIVA = calcularIVA(products[producto].price)
+
+        console.log(`Nombre: ${user} | CUIT: ${cuit}`)
+        console.log(`Producto ${products[producto].product} | Precio unitario: ${precioConIVA} | Cantidad de productos solicitados: ${cantidad} | TOTAL: ${precioConIVA * cantidad}`);
+
+    } else {
+        console.log(`Nombre: ${user} | CUIT: ${cuit}`)
+        console.log(`Producto ${products[producto].product} | Precio unitario: ${products[producto].price} | Cantidad de productos solicitados: ${cantidad} | TOTAL: ${products[producto].price * cantidad}`);
+    }
+}
+
+
+function iniciarEmulador(products) {
+    alert("Comencemos por crear una cuenta!");
+    const username = prompt("Ingresa tu nombre y apellido").toUpperCase();
+    const mail = prompt("Ingresa tu correo electrónico").toLowerCase();
+    const password = prompt("Ingresa tu contraseña").toLowerCase();
+    const passwordConfirmation = prompt("Confirma tu contraseña").toLowerCase();
+
+    // Registramos al usuario y guaramos su nombre.
+    const user = register(username, mail, password, passwordConfirmation);
+
+    // Registramos el pedido del usuario, guardamos el producto desea y la cantidad del mismo.
+    const [producto, cant] = generarPedido(user, products);
+
+    // Generamos la factura de la compra.
+    generarFactura(producto, cant, user, products);
+}
+
+/**
+ * Inicia la emulación de compra y facturación de un usuario en una planta de agua y hielo.
+ * @param {Array} products - Array de productos disponibles
+ */
+iniciarEmulador(products);
