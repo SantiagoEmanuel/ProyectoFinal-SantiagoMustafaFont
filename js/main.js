1248fc6b5d8de1fa282d453633b185dc3b3673fb
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2024-03-20 12:08:54 -0300

    Optimization in reading the code with functions for each action, with documentation of each function

--- js/main.js
+++ js/main.js
@@ -3,16 +3,65 @@ import { login } from "./user/functions/login.js";
 import { logout } from "./user/functions/logout.js";
 
 // Inicializamos la base de datos de los usuarios u obtenemos la misma.
-const users = JSON.parse(localStorage.getItem("usersDB")) || []
+const users = getDataBase()
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Registro - Inicio de sesión
+    // Obtenemos al usuario si es que ha iniciado sesion.
+    const userLogin = getUser()
+
+    // Inicializamos el almacenamiento del carrito de compras.
+    const carrito = getCarrito(userLogin)
+    
+    // Comprobamos el estado del usuario.
+    if (userLogin?.isLogin) {
+        // Mostramos la App.
+        showApp()
+    } else {
+        // Permitimos al usuario iniciar sesion o registrarse.
+        authUser()
+    }
+
+    // Permitimos cerrar sesion al usuario y guardamos su carrito de compras.
+    closeSession(carrito, users, userLogin)
+
+    // Agregamos un producto al carrito de compras del usuario.
+    addProduct(carrito)
+
+    // Mostramos el carrito de compras del usuario.
+    showCarrito(carrito)
+});
+
+/**
+ * Mostramos la App al usuario permitiendole comprar y observar los productos.
+ */
+function showApp() {
+    const userNotLogin = document.getElementById("userNotLogin");
+    const navUserLogin = document.getElementById('navUserIsLogin');
+    const navUserNotLogin = document.getElementById('navUserNotLogin');
+
+    const app = document.getElementById("app");
+
+    userNotLogin.classList.add("hidden");
+    userNotLogin.classList.add('d-none')
+    navUserLogin.classList.remove('hidden');
+    navUserNotLogin.classList.add('hidden');
+
+    app.classList.remove("hidden");
+}
+
+/**
+ * Permitimos al usuario iniciar su sesion o registrarse en nuestra web.
+ */
+function authUser(){
     const form = document.getElementById("form");
     form?.addEventListener("submit", (e) => {
         e.preventDefault();
         if (form.classList.contains("login")) {
-            login(e.target.email.value, e.target.password.value, users);
+            const email = e.target.email.value;
+            const password = e.target.password.value;
+
+            login(email.toLowerCase(), password, users);
         } else {
             const fullName = e.target.name.value;
             const email = e.target.email.value;
@@ -28,37 +77,57 @@ document.addEventListener("DOMContentLoaded", () => {
             );
         }
     });
+}
 
-    // Comprobamos si el usuario inició sesión para mostrar el contenido de la página
-    const userLogin = JSON.parse(localStorage.getItem("userIsLogin"));
-    // Inicializamos el almacenamiento del carrito de compras u obtenemos los valores del usuario.
-    const carrito = JSON.parse(localStorage.getItem('carrito')) ?
-        JSON.parse(localStorage.getItem('carrito')) :
-        (userLogin?.carrito.length != 0 ? userLogin?.carrito : []);
-    if (userLogin?.isLogin) {
-        const userNotLogin = document.getElementById("userNotLogin");
-        const navUserLogin = document.getElementById('navUserIsLogin');
-        const navUserNotLogin = document.getElementById('navUserNotLogin');
-
-        const app = document.getElementById("app");
-
-        userNotLogin.classList.add("hidden");
-        userNotLogin.classList.add('d-none')
-        navUserLogin.classList.remove('hidden');
-        navUserNotLogin.classList.add('hidden');
-
-        app.classList.remove("hidden");
-    }
-
-    // Cerrar sesión del usuario
+/**
+ * Permitimos al usuario cerrar su sesion almacenando su carrito de compras con el usuario para poder recuperarlo una vez inicie sesion nuevamente.
+ * @param {Array} carrito - Carrito de compras del usuario.
+ * @param {Array} users - Lista de usuarios en la base de datos.
+ * @param {Object} user - Usuario con su sesion activa.
+ */
+function closeSession(carrito, users, user){
     const btnLogout = document.getElementById("logout");
     btnLogout?.addEventListener("click", () => {
-        logout(carrito, users, userLogin);
+        logout(carrito, users, user);
     });
+}
 
+/**
+ * Obtenemos de la "Data Base" todos los usuarios.
+ * @returns Lista de usuarios almacenados en DB.
+ */
+function getDataBase() {
+    return JSON.parse(localStorage.getItem("usersDB")) || []
+}
+
+/**
+ * Obtenemos el carrito de compras del usuario previamente guardado, caso contrario inicializamos el carrito de compras vacio.
+ * @param {Object} user - Usuario con su sesion activa.
+ * @returns Carrito de compras del usuario con su sesion activa.
+ */
+function getCarrito(user) {
+    return JSON.parse(localStorage.getItem('carrito')) ?
+    JSON.parse(localStorage.getItem('carrito')) :
+    (user?.carrito.length != 0 ? user?.carrito : []);
+}
+
+/**
+ * Obtenemos al usuario que ha iniciado sesion.
+ * @returns Usuario con la sesion activa.
+ */
+function getUser () {
+    return JSON.parse(localStorage.getItem("userIsLogin"));
+}
+
+/**
+ * Agregamos un producto al carrito de compras del usuario
+ * @param {Array} carrito - Carrito de compras del usuario.
+ */
+function addProduct(carrito) {
     const buttons = document.getElementsByTagName("button");
     for (const btn of buttons) {
         btn.addEventListener("click", () => {
+
             carrito.push(btn.name);
             localStorage.setItem("carrito", JSON.stringify(carrito));
 
@@ -70,7 +139,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
     }
+}
 
+/**
+ * Mostramos el carrito de compras del usuario.
+ * @param {Array} carrito - Carrito de compras del usuario.
+ */
+function showCarrito( carrito ){
     const carritoBtn = document.getElementById('carritoBtn');
     const canvas = document.getElementById('carrito')
     carritoBtn.addEventListener('click', () => {
@@ -124,10 +199,10 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             canvas.innerHTML = `
                 <p class="h5 d-flex flex-column gap-2 align-items-center justify-content-center" style="margin-top: 3rem;">
-                <img src="./assets/error-404.webp" alt="No hay nada que mostrar en tu carrito de compras" width="300"/>
+                <img src="./assets/img/icons/error-404.webp" alt="No hay nada que mostrar en tu carrito de compras" width="300"/>
                     Aún no tienes nada en tu carrito!
                 </p>
             `
         }
     })
-});
+}
